235ac00dfe5900fbaf5242d9eec594e498505f4a
lindan4 <lindx4@gmail.com>
2018-05-18 20:25:25 -0400

    Playing around with operators.

---
+++ arithmetic.rb
@@ -0,0 +1,13 @@
+class Arithmetic
+
+  puts "The sum of 3 and 2 is #{3 + 2}"
+
+
+  #Boolean true or false thankfully
+  puts "Three is greater than two... The answer is #{3 > 2}"
+
+  a = rand(6)
+  b = rand(6)
+
+  
+end
