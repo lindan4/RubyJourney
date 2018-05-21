8940db99bc14acb8812f50835f5dcc46f0a0d38c
lindan4 <lindx4@gmail.com>
2018-05-21 16:29:24 -0400

    Made changes to arithmetic file

--- arithmetic.rb
+++ arithmetic.rb
@@ -6,8 +6,26 @@ class Arithmetic
   #Boolean true or false thankfully
   puts "Three is greater than two... The answer is #{3 > 2}"
 
-  a = rand(6)
-  b = rand(6)
+  a = rand(1...6)
+  b = rand(1...6)
+
+
+  puts "#{a} / #{b} equals #{a / b}"
+
+  #Obtaining float numbers from rand
+  c = rand(1.0...6.0)
+  d = rand(1.0...6.0)
+
+  puts "#{c} / #{d} equals #{c / d}"
+
+  #Rounded to nearest hundrdedth
+  puts "#{c} / #{d} equals (rounded to nearest hundredth) #{(c / d).round(2)}"
+
+
+
+
+
+
 
   
 end
