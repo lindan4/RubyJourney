c30617c5416a0827fb86018961631d69090f8f01
Lindan Thillanayagam <lindx4@gmail.com>
2021-11-13 21:10:36 -0500

    Finished bribing problem.

--- minimum_bribes.rb
+++ minimum_bribes.rb
@@ -1,57 +1,37 @@
 def minimumBribes(q)
     # Write your code here
 
-    minBribes = 0
-
     i = q.length - 1
+
+    localBribe = 0
 
     while i >= 0
         if q[i] != (i + 1)
 
-           localBribe = 0
+            if ((i - 1) >= 0) && (q[i - 1] == (i + 1))
+                temp = q[i - 1]
+                q[i - 1]  = q[i]
+                q[i] = temp
 
+                localBribe = localBribe + 1
 
-           
-           puts q[i]
-        #    puts i + 1
+            elsif ((i - 2) >= 0) && (q[i - 2] == (i + 1))
+                temp = q[i - 2]
+                q[i - 2] = q[i - 1]
+                q[i -1] = q[i]
+                q[i] = temp
 
-            j = i
-
-        #    puts "j: #{j}" 
-        #    puts "q.length #{q.length}"
-
-           while (q[j] != (i + 1)) && (j >= 0)
-            j = j - 1
-            localBribe = localBribe + 1
-           end
-
-
-           
-
-
-           if (j >= 0) 
-            puts q[j]
-            if localBribe > 2
+                localBribe = localBribe + 2
+            else
                 puts 'Too chaotic'
                 return
-            else
-                minBribes = minBribes + localBribe
             end
-
-           end
-
-                  
-
-        #    if q[j] > q[i]
-        #     i = j
-        #    end
-           
         end
         i = i - 1        
     end
     
-    if minBribes > 0
-        puts minBribes
+    if localBribe > 0
+        puts localBribe
     end
 
 
@@ -60,6 +40,7 @@ end
 
 inputArray = [2, 1, 5, 3, 4]
 inputArrayTwo = [1, 2, 5, 3, 7, 8, 6, 4]
+                #0, 1, 2, 3, 4, 5, 6, 7 
 
 
 puts minimumBribes(inputArray)
